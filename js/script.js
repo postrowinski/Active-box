25d4d981c60a7c3c55a6c254b9f2205dbfc056c4
Przemek <p.ostrowinski@gmail.com>
2017-05-26 18:50:07 +0200

    optimise js file

--- js/script.js
+++ js/script.js
@@ -1,62 +1,29 @@
-$(document).ready(function(){
-	
-	//scroll features
-	$('#nav-features').click(function(event){
-		event.preventDefault();
-		var goTop = $('#sec-features').offset().top;
-		$('html, body').animate({
-			scrollTop: goTop
-		}, 700);
-	});
-	
-	//scroll works
-	$('#nav-works').click(function(event){
-		event.preventDefault();
-		var goTop = $('#sec-works').offset().top;
-		$('html, body').animate({
-			scrollTop: goTop
-		}, 700);
-	});
-	
-	// scroll team
-	$('#nav-team').click(function(event){
-		event.preventDefault();
-		var goTop = $('#sec-team').offset().top;
-		$('html, body').animate({
-			scrollTop: goTop
-		}, 700);
-	});
-	
-	//scroll clients
-	$('#nav-clients').click(function(event){
-		event.preventDefault();
-		var goTop = $('#sec-clients').offset().top;
-		$('html, body').animate({
-			scrollTop: goTop
-		}, 700);
-	});
-	
-	//scroll download
-	$('#nav-download').click(function(event){
-		event.preventDefault();
-		var goTop = $('#sec-download').offset().top;
-		$('html, body').animate({
-			scrollTop: goTop
-		}, 700);
-	});
-	
-	//scroll website top
-	$('.scroll-top').click(function(event){
-		event.preventDefault();
-		var goTop = $('html').offset().top;
-		$('html, body').animate({
-			scrollTop: goTop
-		}, 700);
-	});
-	
-	//visibility .scroll-top button
-	$(window).scroll(function(){
-    	if($(this).scrollTop()>350) $('.scroll-top').fadeIn();
+$(document).ready(function () {
+
+    //navigation scroll down
+    $('nav a').click(function (event) {
+        event.preventDefault();
+        var getLink = $(this).attr('href'),
+            goTop = $(getLink).offset().top;
+
+        $('html, body').animate({
+            scrollTop: goTop
+        }, 700);
+    });
+    
+    //scroll website top from bottom-right arrow
+    $('.scroll-top').click(function (event) {
+        event.preventDefault();
+        var goTop = $('html').offset().top;
+
+        $('html, body').animate({
+            scrollTop: goTop
+        }, 700);
+    });
+
+    //visibility bottom-right arrow
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 350) $('.scroll-top').fadeIn();
         else $('.scroll-top').fadeOut();
     });
 });
